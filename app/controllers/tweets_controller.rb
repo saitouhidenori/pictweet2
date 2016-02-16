8830a5f0252d20c167e18c5ded01ad7d0e31478c
saitou hidenori <saitouhidenori@MacBook-Pro-saito.local>
2016-02-16 22:49:03 +0900

    edit前まで編集済み

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,19 +1,30 @@
 class TweetsController < ApplicationController
 
   def index
-    @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
+    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
   end
 
   def new
   end
 
   def create
-    Tweet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+  end
+
+  def edit
+    @tweet= Tweet.find(params[:id])
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.destroy
+    end
   end
 
   private
   def tweet_params
-    params.permit(:name, :image, :text,)
+    params.permit(:image, :text,)
   end
 
 
